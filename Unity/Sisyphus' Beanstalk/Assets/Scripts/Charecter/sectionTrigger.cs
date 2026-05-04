c017d7bd0cd99119eadafa49c13f8c9849b6db52
MrBigBrain0 <148355534+MrBigBrain0@users.noreply.github.com>
2026-05-04 17:21:59 -0400

    WarmUp_Programing_Update
    Changes made:
    - deleted old movement script
    - fixing platform

---
+++ Unity/Sisyphus' Beanstalk/Assets/Scripts/Charecter/sectionTrigger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class sectionTrigger : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Unity/Sisyphus' Beanstalk/Assets/Scripts/Level/Scene Scripts/GameManager.cs
+++ Unity/Sisyphus' Beanstalk/Assets/Scripts/Level/Scene Scripts/GameManager.cs
@@ -1,22 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject platformPrefab;
 
-    public int platformCount = 300;
+    public float platformCount = 10;
 
     private void Start()
     {
+
+
         Vector3 spawnPosition = new Vector3();
 
-        for(int i = 0; i < platformCount; i++)
+        for (int i = 0; i < platformCount; i++)
         {
             spawnPosition.y += Random.Range(.5f, 2f);
             spawnPosition.x = Random.Range(-5f, 5f);
             Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
         }
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+
+
     }
 }
