2871e734cbdddb0c1a9ca85d68d2963ebfd63d5d
MrBigBrain0 <148355534+MrBigBrain0@users.noreply.github.com>
2026-05-04 16:11:10 -0400

    WarmUp_Programming_Update
    Changes made:
    - added the random platform generation

--- Unity/Sisyphus' Beanstalk/Assets/Scripts/Level/Scene Scripts/GameManager.cs
+++ Unity/Sisyphus' Beanstalk/Assets/Scripts/Level/Scene Scripts/GameManager.cs
@@ -1,16 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject platformPrefab;
 
-    // Update is called once per frame
-    void Update()
+    public int platformCount = 300;
+
+    private void Start()
     {
-        
+        Vector3 spawnPosition = new Vector3();
+
+        for(int i = 0; i < platformCount; i++)
+        {
+            spawnPosition.y += Random.Range(.5f, 2f);
+            spawnPosition.x = Random.Range(-5f, 5f);
+            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+        }
     }
 }
